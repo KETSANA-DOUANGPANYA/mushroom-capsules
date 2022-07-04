144ab87cba7ab1d33d5e1f3693e8cd5909c88277
KETSANA-DOUNAGPANYA <ketsanadpy@gmai.com>
2022-07-04 16:53:00 +0700

    update now input

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ float temps[2];
 // Note that older versions of this library took an optional third parameter to
 // tweak the timings for faster processors.  This parameter is no longer needed
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
-DHT dht[] = {{4, DHTTYPE}, {5, DHTTYPE}};
+DHT dht[] = {{5, DHTTYPE}, {16, DHTTYPE}};
 void setup()
 {
   Serial.begin(115200);
